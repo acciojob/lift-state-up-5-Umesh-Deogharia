c54ec14a981135beb115919170f8a2468306ad54
Umesh-Deogharia <umeshdeogharia07@gmail.com>
2025-04-02 23:30:19 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
+      <h1>Parent Component</h1>
       {isLoggedIn ? <h1>You are logged in</h1> :
         <Login setIsLoggedIn={setIsLoggedIn} />
       }
